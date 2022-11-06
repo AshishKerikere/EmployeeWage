3525294bbcdda10dbee2da556798a738d5a85177
AshishKerikere <93969829+AshishKerikere@users.noreply.github.com>
2022-11-06 16:11:26 +0530

    Updated EmployeeWageComputationProgram till UC1

--- EmployeeWageComputationProgram.java
+++ EmployeeWageComputationProgram.java
@@ -1,1 +1,16 @@
+public class EmployeeWageComputationProgram {
+    public static void main(String[] args){
 
+        String attendance;
+        int random = (int)Math.floor(Math.random()*2+1);
+
+        if (random == 1){
+            attendance = "Absent";
+        }
+        else{
+            attendance = "Present";
+        }
+
+        System.out.println("The employee is " +attendance);
+    }
+}

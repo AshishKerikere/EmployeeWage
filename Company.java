2ee4959c092d38040824f2060b8deff8a866ae51
AshishKerikere <93969829+AshishKerikere@users.noreply.github.com>
2022-11-06 16:12:28 +0530

    Updated Company Class till UC1

--- Company.java
+++ Company.java
@@ -1,1 +1,14 @@
+public class Company {
+    String name_ofthe_Company = "";
+    int wage_Per_Hour = 0;
+    int max_Working_Hours = 0;
+    int max_Working_days = 0;
+    int total_Wage_Paid = 0;
+    public Company(String name_ofthe_Company, int wage_Per_Hour, int max_Working_Hours, int max_Working_days){
+        this.name_ofthe_Company = name_ofthe_Company;
+        this.wage_Per_Hour = wage_Per_Hour;
+        this.max_Working_Hours = max_Working_Hours;
+        this.max_Working_days = max_Working_days;
+    }
+}
 
